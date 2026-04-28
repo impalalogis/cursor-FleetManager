0db23fc098928008c21ff4494f4bfcab741b513e
Aniket Singh <impalalogis@example.com>
2026-04-28 12:10:39 -0400

    url details

--- FleetManager/settings.py
+++ FleetManager/settings.py
@@ -97,7 +97,9 @@ INSTALLED_APPS = [
     'users',
     'configuration',
     'rest_framework',
-    'drf_spectacular'
+    'drf_spectacular',
+    'corsheaders',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -108,7 +110,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [],
